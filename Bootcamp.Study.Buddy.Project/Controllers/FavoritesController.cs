9787cd7f662863858d2208d5629329b88c9fe327
Billy-McCleese <billymccleese@gmail.com>
2023-06-01 21:30:39 -0400

    First push

---
+++ Bootcamp.Study.Buddy.Project/Controllers/FavoritesController.cs
@@ -0,0 +1,23 @@
+﻿using Bootcamp.Study.Buddy.Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Bootcamp.Study.Buddy.Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly StudentBootCampDbContext _dbContext;
+        public QuestionsController(StudentBootCampDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllQuestions()
+        {
+            var questions = _dbContext.Questions.ToList();
+            return Ok(questions);
+        }
+    }
+}
---
+++ Bootcamp.Study.Buddy.Project/Models/StudentBootCampDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootcamp.Study.Buddy.Project.Models;
+
+public partial class StudentBootCampDbContext : DbContext
+{
+    public StudentBootCampDbContext()
+    {
+    }
+
+    public StudentBootCampDbContext(DbContextOptions<StudentBootCampDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Favorite> Favorites { get; set; }
+
+    public virtual DbSet<Question> Questions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress07;Database=StudentBootCampDB;Trusted_Connection=True; Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Favorite>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Favorite__3214EC0746F24122");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<Question>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Question__3214EC076BFD9ECC");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Answer).HasMaxLength(200);
+            entity.Property(e => e.QuestionTitle).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
