134ec2a57a7e7f10b5f40f2ff4d152d5ad5ddb2e
asmarazaq <45542493+asmarazaq@users.noreply.github.com>
2023-06-03 13:23:17 -0400

    added question detail (#3)
    Co-authored-by: Asma Razaq <asmarazaq@rocketmortgage.com>

--- BootCampStudyClient/src/app/question-detail-component/question-detail-component.component.ts
+++ BootCampStudyClient/src/app/question-detail-component/question-detail-component.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Question } from '../question';
 
 @Component({
   selector: 'app-question-detail-component',
   templateUrl: './question-detail-component.component.html',
   styleUrls: ['./question-detail-component.component.css']
 })
-export class QuestionDetailComponentComponent {
+export class QuestionDetailComponentComponent  implements OnInit {
 
+  selectedQuestion: Question | undefined;
+  // will Replace with the actual ID of the selected question
+  selectedQuestionId: number = 5; 
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getQuestionById(this.selectedQuestionId);
+  }
+
+  getQuestionById(id: number) {
+    this.apiService.getQuestionById(id).subscribe(
+      (question: Question) => {
+        this.selectedQuestion = question;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+// calling the API service to mark the question as a favorite
+   
+  markAsFavorite(question: Question) {
+    
+  }
 }
